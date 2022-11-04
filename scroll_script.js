1620f55ba9f0c089b3d4ce2d2fef0ce45e82f22f
edward frempong <asamoah.frempong@gmail.com>
2022-11-04 14:54:13 +0000

    updated navbar and rm download price

--- scroll_script.js
+++ scroll_script.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         {
           scrollTop: $(hash).offset().top,
         },
-        900,
+        200,
         function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
